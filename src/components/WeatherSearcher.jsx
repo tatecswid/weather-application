ef4f20d3c2f47234e71e9273ba6dd730ad6e7c52
tatecswid <tatecswid@gmail.com>
2026-04-27 00:16:39 -0400

    add search feature

--- src/components/WeatherSearcher.jsx
+++ src/components/WeatherSearcher.jsx
@@ -1,9 +1,28 @@
 import "../App.css"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const WeatherSearcher = (props) => {
     const [cityName, setCityName] = useState("");
+    const [dropDownMenu, setDropDownMenu] = useState([]);
+
+    useEffect(() => {
+        fetchDropDownCities();
+    }, [])
+
+    const fetchDropDownCities = async () => {
+        const res = await fetch("https://gist.githubusercontent.com/ahmu83/38865147cf3727d221941a2ef8c22a77/raw/c647f74643c0b3f8407c28ddbb599e9f594365ca/US_States_and_Cities.json")
+        const data = await res.json();
+        const locations = Object.entries(data);
+        const reformatedLocations = [];
+        
+        for(var location of locations) {
+            for(let i = 0; i < location[1].length; i++) {
+                reformatedLocations.push(location[1][i] + ", " + location[0])
+            }  
+        }
+        setDropDownMenu(reformatedLocations);
+    }
 
     const fetchWeatherData = async () => {
         props.resetErrorState();
@@ -11,7 +30,6 @@ export const WeatherSearcher = (props) => {
         try {
             props.setLoading(true);
             const res = await fetch(`/.netlify/functions/weather?city=${cityName}`);
-            
 
             if(!res.ok) {
                 throw new Error("Failed to fetch weather data");
@@ -32,8 +50,22 @@ export const WeatherSearcher = (props) => {
     return (
         <div className="weather-searcher">
             <div className="weather-searcher-inner">
-                <input type="text" onChange={(e) => setCityName(e.target.value)} maxLength={34} placeholder="City, State"/>
+                <input type="text" value={cityName} onChange={(e) => setCityName(e.target.value)} maxLength={34} placeholder="City, State"/>
                 <input onClick={fetchWeatherData} type="submit" value="Get Weather"/>
+            </div>
+            <div className="dropdown-menu">
+                {dropDownMenu?.filter((dropCityName) => {
+                    return cityName !== "" && 
+                    dropCityName.toLowerCase().startsWith(cityName.toLowerCase()) && 
+                    dropCityName.toLowerCase() !== cityName.toLowerCase();
+                })
+                .slice(0,10)
+                .map((dropCityName) => (
+                    <div className="dropdown-item" key={dropCityName} onClick={(e) => {
+                        setCityName(dropCityName);
+                        console.log(dropCityName);
+                    }}>{dropCityName}</div>
+                ))}
             </div>
         </div>
     );
